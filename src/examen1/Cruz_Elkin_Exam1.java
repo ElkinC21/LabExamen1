129d2f16fbc63a96c7adf3c8e35b5ed565a7a89d
elkin <elkin@DESKTOP-KQKAO9U>
2025-05-23 21:11:40 +0200

    PUSH INICIAL

---
+++ src/examen1/Cruz_Elkin_Exam1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package examen1;
+import java.util.Scanner;
+/**
+ *
+ * @author elkin
+ */
+public class Cruz_Elkin_Exam1 {
+    public static void main(String[] args) {
+        
+   Scanner lea= new Scanner(System.in);
+    
+        System.out.println("------MENU-------"); 
+        System.out.println("1. Piramide");
+        System.out.println("2. Clave");
+        System.out.println("3. Piedra Papel o Tijera");
+        System.out.println("4. Adivinar");
+    
+    }
+   
+}
